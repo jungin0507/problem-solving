21bad3cb7da89f0b274d3f2461772ea5c87261c3
jungin0507 <a01033046758@gmail.com>
2023-02-04 15:49:30 +0900

    shell script 변경

--- git_push.sh
+++ git_push.sh
@@ -26,16 +26,28 @@ number='2'
 while [ $number != '1' ]
 do
   echo "출처 사이트를 입력해주세요."
-  read site
+  echo "1. BOJ"
+  echo "2. Codeforces"
+  read site_selection
+  if [ $site_selection == '1' ]
+  then
+    site="BOJ"
+  elif [ $site_selection == '2' ]
+  then
+    site="Codeforces"
+  else
+    echo "잘못된 입력입니다."
+    continue
+  fi
 
   echo "$site 이(가) 맞습니까?"
   echo "[1: 예 / 2: 아니오]"
   read number
 done
 
-mkdir -p ./src/${site}/${category} && cp ./base.cpp ./src/${site}/${category}/${prob}.cpp
-echo "src/${site}/${category}/${prob}.cpp에 저장되었습니다."
-git add ./src/${site}/${category}/${prob}.cpp
+mkdir -p ./${site}/${category} && cp ./base.cpp ./${site}/${category}/${prob}.cpp
+echo "${site}/${category}/${prob}.cpp에 저장되었습니다."
+git add ./${site}/${category}/${prob}.cpp
 git commit -m "${site}_${prob}"
 git push origin main
 echo "${site}/${category}/${prob}.cpp 파일 커밋이 완료되었습니다."
