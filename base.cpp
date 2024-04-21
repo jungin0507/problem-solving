8eec4a14efe29117b0546049da1d18689d63adea
jungin0507 <a01033046758@gmail.com>
2024-04-21 16:42:13 +0900

    change settings

--- base.cpp
+++ base.cpp
@@ -7,12 +7,12 @@
 #define all(x) x.begin(), x.end()
 #define rep(i, n) for (auto i = 0; i < (n); ++i)
 #define rrep(i, n) for (auto i = (n) -1; i >= 0; --i)
-#define fastio                                 \
-    {                                          \
-        ios::ios_base::sync_with_stdio(false); \
-        cin.tie(0);                            \
-        cout.tie(0);                           \
-    }
+#define fastio                             \
+  {                                        \
+    ios::ios_base::sync_with_stdio(false); \
+    cin.tie(0);                            \
+    cout.tie(0);                           \
+  }
 
 using namespace std;
 using ll = long long;
@@ -26,53 +26,53 @@ using pli = pair<ll, int>;
 /* operators of std::pair<T1, T2> */
 template<class T1, class T2>
 ostream &operator<<(ostream &o, pair<T1, T2> x) {
-    return o << x.first << ' ' << x.second;
+  return o << x.first << ' ' << x.second;
 }
 template<class T1, class T2>
 istream &operator>>(istream &i, pair<T1, T2> &x) {
-    return i >> x.first >> x.second;
+  return i >> x.first >> x.second;
 }
 template<class T1, class T2>
 pair<T1, T2> operator+(pair<T1, T2> x, pair<T1, T2> y) {
-    return {x.first + y.first, x.second + y.second};
+  return {x.first + y.first, x.second + y.second};
 }
 template<class T1, class T2>
 pair<T1, T2> operator-(pair<T1, T2> x, pair<T1, T2> y) {
-    return {x.first - y.first, x.second - y.second};
+  return {x.first - y.first, x.second - y.second};
 }
 template<class T1, class T2>
 void operator+=(pair<T1, T2> &x, pair<T1, T2> y) {
-    x = x + y;
+  x = x + y;
 }
 template<class T1, class T2>
 void operator-=(pair<T1, T2> &x, pair<T1, T2> y) {
-    x = x - y;
+  x = x - y;
 }
 
 /* operators of std::vector<T> */
 template<class T>
 ostream &operator<<(ostream &o, vector<T> v) {
-    for (auto it = v.begin(); it != v.end(); ++it) {
-        o << (*it) << ' ';
-    }
-    return o;
+  for (auto it = v.begin(); it != v.end(); ++it) {
+    o << (*it) << ' ';
+  }
+  return o;
 }
 template<class T>
 istream &operator>>(istream &i, vector<T> &v) {
-    for (auto it = v.begin(); it != v.end(); ++it) {
-        i >> (*it);
-    }
-    return i;
+  for (auto it = v.begin(); it != v.end(); ++it) {
+    i >> (*it);
+  }
+  return i;
 }
 void solve();
 void preproc();
 
 signed main() {
-    fastio;
-    int T = 1;
-    // cin >> T;
-    preproc();
-    while (T-- > 0) solve();
+  fastio;
+  int T = 1;
+  // cin >> T;
+  preproc();
+  while (T-- > 0) solve();
 }
 
 /* actual code */
